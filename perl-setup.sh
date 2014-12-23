ed3a00559add6292e544dcb9f3a3237c734bc2be
Erlend Leganger <erlend.leganger@ssc.nato.int>
2014-12-23 09:16:10 +0100

    Added output during execution

--- perl-setup.sh
+++ perl-setup.sh
@@ -1,14 +1,23 @@
+me=$(basename $0)
+echo $me: start
+
 #ensure the gcc programs exist in a *-4 version as well
+printf "\n$me: set gcc names\n"
 for s in gcc g++; do 
-   src="/usr/bin/$src"
-   tgt="/usr/bin/$src-4"
+   src="/usr/bin/$s"
+   tgt="/usr/bin/$s-4"
    [ ! -f $tgt ] && ln -s $src $tgt
 done
 
+printf "\n$me: get cpanm:\n"
 #get cpanm, for module maintenance
 curl -L http://cpanmin.us|perl - App::cpanminus;
 
 #install required perl modules
 for m in Error Log::Log4perl XML::Parser; do 
+   printf "\n$me: install perl module $m:\n"
    cpanm -fv $m
 done
+
+#done
+echo $me: done
