8836584e68acc0b473ce1c96f731a763994a4029
Erlend Leganger <erlend.leganger@gmail.com>
2015-03-28 17:56:50 +0100

    Added credential winstore for cygwin git

--- cygwin-setup.cmd
+++ cygwin-setup.cmd
@@ -1,23 +1,23 @@
 @echo off
+::------------------------------------------------------------------------
 ::get my file name, no extension
 ::set me=%~n0 
 ::get my file name, including extension:
 set me=%~n0%~x0 
 ::get directory of this file, including trailing \
 set bindir=%~dp0
-
-echo %me%: start
+set line=---------------------------------------------------------------------
 :: --quiet-mode --no-startmenu --no-desktop --no-shortcuts 
 echo %me%: initialise configuration:
 set pkgdir=c:\original\cygwin
 set rootdir=c:\opt\cygwin
 set srcsite=http://cygwin.uib.no/ 
-
 :: use 32bit version, 64bit not yet mature (Jan 2015)
 ::set setup=setup-x86_64.exe
 set setup=%bindir%setup-x86.exe
 
-echo %me%: define list of packages to install, update:
+::------------------------------------------------------------------------
+::define list of packages to install, update:
 set pkgs=binutils
 set pkgs=%pkgs%,bzip2
 set pkgs=%pkgs%,colorgcc
@@ -43,11 +43,12 @@ set pkgs=%pkgs%,libxml2-devel
 set pkgs=%pkgs%,libexpat-devel
 set pkgs=%pkgs%,libcrypt-devel
 
+::------------------------------------------------------------------------
 echo %me%: start installer, do the job:
 %setup%  --quiet-mode --wait --no-desktop --local-package-dir %pkgdir% --site %srcsite% --root %rootdir% --upgrade-also --packages %pkgs%
 
+::------------------------------------------------------------------------
 ::do the perl stuff
-set line=---------------------------------------------------------------------
 echo[
 echo %line%
 set script=perl-setup.sh
@@ -59,21 +60,28 @@ echo %me%: - run 'time bash /tmp/%script%'
 echo %me%: - wait, the job can take a few minutes
 echo %me%: configure perl: end
 echo %line%
+
+::------------------------------------------------------------------------
+::do the putty stuff
+set exe=git-credential-winstore.exe
+copy %bindir%%exe% %rootdir%\usr\bin >nul
 set script=puttycyg-setup.sh
 copy %bindir%%script% %rootdir%\tmp >nul
 echo %me%: configure Putty: start
 echo %me%: - run 'time bash /tmp/%script%'
 echo %me%: - wait, the job takes a few seconds
-echo %me%: - suggested shortcut target: "C:\opt\putty\putty.exe -load cygwin"
+echo %me%: - suggested shortcut target: C:\opt\putty\putty.exe -load cygwin
 echo %me%: configure Putty: end
 echo %line%
 echo %me%: - when done, close the terminal window with 'exit'
 
+::------------------------------------------------------------------------
 ::start a terminal for the commands above
 ::mintty - alternative terminal emulator
 ::%rootdir%\bin\mintty.exe -
 %rootdir%\bin\run.exe -wait /usr/bin/rxvt.exe -display 127.0.0.1:0 -tn rxvt-cygwin -e /bin/bash --login
 
+::------------------------------------------------------------------------
 ::ok, all done now
 echo[
 echo %me%: cygwin installation completed
