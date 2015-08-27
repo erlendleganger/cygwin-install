5a1761064696f6bb51c043fb5681b1127af6df6e
Erlend Leganger <erlend.leganger@gmail.com>
2015-08-27 07:32:20 +0200

    Added error checking

--- src/perl-config.sh
+++ src/perl-config.sh
@@ -1,3 +1,4 @@
+line=------------------------------------------------------------------------
 me=$(basename $0)
 echo $me: start
 
@@ -15,8 +16,15 @@ curl -L http://cpanmin.us|perl - App::cpanminus;
 
 #install required perl modules
 for m in Error Log::Log4perl XML::Parser XML::Simple; do 
-   printf "\n$me: install perl module $m:\n"
+   printf "\n$line\n$me: install perl module $m:\n"
    cpanm -fv $m
+   perldoc -l $m >/dev/null 2>&1
+   if [ $? != 0 ]; then
+      echo error: installation of module $m failed
+      exit 1
+   else
+      echo info: installation of module $m ok
+   fi
 done
 
 #done
