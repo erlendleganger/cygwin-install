2e93f872d4ac5e8c70738125d5cf752bdc0d389d
Erlend Leganger <erlend.leganger@gmail.com>
2015-08-27 08:26:17 +0200

    Reworked to run out of current directory

--- src/puttycyg-setup.sh
+++ src/puttycyg-setup.sh
@@ -1,15 +1,14 @@
 #------------------------------------------------------------------------
 #settings
-tgtdir=/cygdrive/c/opt/putty
-pkgdir=/cygdrive/c/original/cygwin-install
-srcfile=$pkgdir/puttycyg-20101029.zip
+tgtdir=/c/opt/putty
+srcfile=puttycyg-20101029.zip
 puttycygdir=puttycyg-20101029
 
 #------------------------------------------------------------------------
+cd $(dirname $0)
+
+#------------------------------------------------------------------------
 #initial checks
-if [ ! -d $pkgdir ]; then
-   echo cannot find $pkgdir; exit 1
-fi
 if [ ! -f $srcfile ]; then
    echo cannot find $srcfile; exit 1
 fi
@@ -19,10 +18,10 @@ echo initialise $tgtdir
 rm -rf $tgtdir
 mkdir -p $tgtdir
 echo unzip $srcfile
-cd /tmp
 rm -rf $puttycygdir
 unzip $srcfile
 echo copy files to $tgtdir
 cp -p $puttycygdir/* $tgtdir
+rm -rf $puttycygdir
 find $tgtdir -type f
 echo done
