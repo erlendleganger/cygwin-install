e4eb5c490e0628495d1fabb34e2deabea15c8213
Erlend Leganger <erlend.leganger@gmail.com>
2015-08-27 08:26:32 +0200

    Use cygwin resource directory instead of tmp

--- cygwin-setup.cmd
+++ cygwin-setup.cmd
@@ -1,5 +1,16 @@
 @echo off
 ::------------------------------------------------------------------------
+::configure pkgdir, rootdir, srcsite as required below
+set id=cygwin
+::srcsite - the cygwin mirror to pull packages from 
+set srcsite=http://cygwin.uib.no/ 
+::pkgdir - location for all cygwin packages
+set pkgdir=c:\original\%id%
+::rootdir - cygwin target directory, this is where cygwin will run from 
+set rootdir=c:\opt\%id%
+
+::------------------------------------------------------------------------
+echo %me%: initialise configuration:
 ::get my file name, no extension
 ::set me=%~n0 
 ::get my file name, including extension:
@@ -9,13 +20,9 @@ set basedir=%~dp0
 set xferdir=%basedir%xfer
 set bindir=%basedir%bin
 set srcdir=%basedir%src
+set resdir=%rootdir%\opt\cygwin\src
+set resdircyg=/opt/cygwin/src
 set line=---------------------------------------------------------------------
-:: --quiet-mode --no-startmenu --no-desktop --no-shortcuts 
-echo %me%: initialise configuration:
-set id=cygwin
-set pkgdir=c:\original\%id%
-set rootdir=c:\opt\%id%
-set srcsite=http://cygwin.uib.no/ 
 :: use 32bit version, 64bit not yet mature (Jan 2015)
 ::set setup=setup-x86_64.exe
 set setup=%bindir%\setup-x86.exe
@@ -58,15 +65,22 @@ echo %me%: start installer, do the job:
 %setup%  --quiet-mode --wait --no-desktop --local-package-dir %pkgdir% --site %srcsite% --root %rootdir% --upgrade-also --packages %pkgs%
 
 ::------------------------------------------------------------------------
+::copy misc files in place
+mkdir %resdir%
+copy %srcdir%\vimrc.txt %resdir% >nul
+copy %srcdir%\fstab %rootdir%\etc >nul
+
+::------------------------------------------------------------------------
 ::do the cygwin stuff
 echo[
 echo %line%
 set script=cygwin-config.sh
-copy %srcdir%\%script% %rootdir%\tmp >nul
+copy %srcdir%\%script% %resdir% >nul
+copy %srcdir%\git-credential-winstore.exe %resdir% >nul
 echo %me%: - observe that a terminal window now is shown
 echo %line%
 echo %me%: do this to configure Cygwin:
-echo %me%: - run 'time bash /tmp/%script%'
+echo %me%: - run 'time bash %resdircyg%/%script%'
 echo %me%: - wait, the job takes a few seconds
 echo %me%: - done
 echo %line%
@@ -76,11 +90,11 @@ echo %line%
 echo[
 echo %line%
 set script=perl-config.sh
-copy %srcdir%\%script% %rootdir%\tmp >nul
+copy %srcdir%\%script% %resdir% >nul
 echo %me%: - observe that a terminal window now is shown
 echo %line%
 echo %me%: do this to configure Perl:
-echo %me%: - run 'time bash /tmp/%script%'
+echo %me%: - run 'time bash %resdircyg%/%script%'
 echo %me%: - wait, the job can take a few minutes
 echo %me%: - done
 echo %line%
@@ -90,14 +104,16 @@ echo %line%
 set exe=git-credential-winstore.exe
 copy %srcdir%\%exe% %rootdir%\usr\bin >nul
 set script=puttycyg-setup.sh
-copy %srcdir%\%script% %rootdir%\tmp >nul
-echo %me%: do this to configure Putty:
-echo %me%: - run 'time bash /tmp/%script%'
-echo %me%: - wait, the job takes a few seconds
-echo %me%: - suggested shortcut target: C:\opt\putty\putty.exe -load cygwin
-echo %me%: configure Putty: end
-echo %line%
-echo %me%: - when done, close the terminal window with 'exit'
+copy %srcdir%\%script% %resdir% >nul
+copy %srcdir%\puttycyg-20101029.zip %resdir% >nul
+copy %srcdir%\putty-cygwin-session.reg %resdir% >nul
+::echo %me%: do this to configure Putty:
+::echo %me%: - run 'time bash %resdircyg%/%script%'
+::echo %me%: - wait, the job takes a few seconds
+::echo %me%: - suggested shortcut target: C:\opt\putty\putty.exe -load cygwin
+::echo %me%: configure Putty: end
+::echo %line%
+::echo %me%: - when done, close the terminal window with 'exit'
 
 ::------------------------------------------------------------------------
 ::start a terminal for the commands above
